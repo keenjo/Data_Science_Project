8e07158eb30fb8b15563869d60ce691a137e2de3
keenjo <90385673+keenjo@users.noreply.github.com>
2022-04-09 18:25:22 +0200

    Fixes to get test of 400 articles to work

--- scraping_functions.py
+++ scraping_functions.py
@@ -1,12 +1,23 @@
 import wptools
 import wikipedia
 import pandas as pd
+import numpy as np
+import re
+from tqdm import tqdm
+
+#%%
+#I've changed the names of some of these categories from plural to singular because the API gives much better results
+# For instance, with 'Airports' I get a lot of list articles but with 'Airport' I get articles about actual airports
+
+categories = ['Airport', 'Artist', 'Astronaut', 'Building', 'Astronomical Object',
+              'City', 'Comicbook Character', 'Company', 'Food', 'Transport', 'Monument',
+              'Politician', 'Team', 'Athlete', 'University', 'Written Communication']
+
 
 #%%
 
 def get_articles(cat_list, num_results):
-    # Add parameter to check number of sentences perarticle (either here or in 'get_content' function)
-    # See if we can fix this function to skip 'list' and 'disambiguation' wikipedia pages
+    # **Add parameter to check number of sentences per article (either here or in 'get_content' function)
     # Tried creating this function with RDF/SPARQL but had trouble looping through queries for each category
     
     '''
@@ -19,8 +30,16 @@ def get_articles(cat_list, num_results):
     
     articles_total = [] # List containing a list of articles for each category
     
-    for cat in cat_list:
+    for cat in tqdm(cat_list, desc='Retrieving articles'):
         new_articles = wikipedia.search(f'{cat}', results=num_results)
+        for article in new_articles:
+            if re.search('list', article.lower()):
+                new_articles.remove(article)
+            elif re.search('lists', article.lower()):
+                new_articles.remove(article)
+            elif re.search('disambiguation', article.lower()):
+                new_articles.remove(article)
+                
         articles_total.append(new_articles)
         
     return articles_total
@@ -36,11 +55,13 @@ def get_titles_info(article_list):
     
     titles_total = [] # Contains all of the titles 
     infoboxes_total = [] # infoboxes of the pages
+    counter = 0
     
     for cat in article_list: # For every category in article_list
         cat_titles = []
         cat_boxes = []
-        for art in cat: # For every article in a specific category
+        counter += 1
+        for art in tqdm(cat, desc=f'Retrieving titles & infoboxes ({counter}/{len(article_list)})'): # For every article in a specific category
             page = wptools.page(art)
             page.get_parse()
             page_name = page.data['title']
@@ -50,10 +71,10 @@ def get_titles_info(article_list):
             if page.data['infobox']:
                 page_infobox = page.data['infobox']
                 cat_boxes.append(page_infobox)
+                print(f'{art} infobox retrieved')
             else:
                 cat_boxes.append(None)
-                
-            print(f'{art} infobox retrieved')
+                print(f'{art} does not have an infobox')
             
         titles_total.append(cat_titles)
         infoboxes_total.append(cat_boxes)
@@ -63,7 +84,7 @@ def get_titles_info(article_list):
     
 #%%
 
-def get_content(title_list, infobox_list):
+def get_content(titles_total, infoboxes_total):
 
     '''
     This function gets the content from all of the article titles in the total_titles list
@@ -73,15 +94,52 @@ def get_content(title_list, infobox_list):
 
     content_total = [] # content of the pages
 
-    for cat in title_list: # For every category in title_list
+    for cat_index, cat in enumerate(titles_total): # For every category in title_list
         cat_cont_list = [] # List of all of the content for a specific category
-        for title in cat: # For every title in a specific category
-            page = wptools.page(title)
-            page.get_query()
-            page_content = page.data['extext']
-            cat_cont_list.append(page_content)
-            print(f'{title} content retrieved')
+        for title_index, title in tqdm(enumerate(cat), desc=f'Retreiving content ({cat_index + 1}/{len(titles_total)})'): # For every title in a specific category
+            try:
+                page = wikipedia.page(title)
+                cat_cont_list.append(page.content)
+                print(f'{title} content retrieved')
+            except wikipedia.exceptions.PageError:
+                # ***Don't love this solution but don't know if there is an alternative to solve PageError***
+                cat_cont_list.append(None)
+                print(f'{title} content not found')
+            except wikipedia.exceptions.DisambiguationError: #as many:
+                cat_cont_list.append(None)
+                print(f'{title} content not found')
+                # Had a double disambiguation error which broke the code below, working on solution
+                
+                '''
+                This bit will choose a random value from the disambiguation options then do the following:
+                    - update the information in the title and infoboxes lists
+                    - retrieve the content for this new article
+                    
+                NOTE TO SELF: Make this bit more efficient by simplifying functions and making this bit recursive
+                - instead of having all of these functions only work on a list of articles,
+                  make them work on one article then just create another function which loops through this simpler funtion
+                
+                updated_item = many.options[np.random.randint(0, len(many.options))]
+                
+                page = wptools.page(updated_item)
+                page.get_parse()
+                page_name = page.data['title']
+                titles_total[cat_index][title_index] = page_name 
+                print(f'UPDATE: {title} title retrieved')
             
+                if page.data['infobox']:
+                    page_infobox = page.data['infobox']
+                    infoboxes_total[cat_index][title_index] = page_infobox
+                    print(f'UPDATE: {title} infobox retrieved')
+                else:
+                    infoboxes_total[cat_index][title_index] = None
+                    print(f'UPDATE: {title} does not have an infobox')
+                
+                page = wikipedia.page(updated_item)
+                cat_cont_list.append(page.content)
+                
+                print(f'{title} content retrieved')
+                '''
         content_total.append(cat_cont_list)
         
     return content_total
@@ -99,41 +157,46 @@ def get_triples(page_name):
     triples_list = [] # List of all of the triples for one article
     
     page = wptools.page(page_name, silent=True)
-    page.get_wikidata()
     
-    for x in page.data['wikidata']:
-        one_triple = []
-        #print(type(page.data['wikidata'][x]))
-        if type(page.data['wikidata'][x]) is str:
-            one_triple.append(x)
-            one_triple.append(page.data["wikidata"][x])
-            triples_list.append(one_triple)
+    try:
+        page.get_wikidata()
+        
+        for x in page.data['wikidata']:
             one_triple = []
-            #print(f'{x}; {page.data["wikidata"][x]}\n')
-        if type(page.data['wikidata'][x]) is list:
-            for item in page.data['wikidata'][x]:
-                if type(item) is str:
-                    one_triple.append(x)
-                    one_triple.append(item)
-                    triples_list.append(one_triple)
-                    one_triple = []
-                    #print(f'{x}; {item}\n')
-                elif type(item) is dict:
-                    for key in item:
-                        one_triple.append(x)
-                        one_triple.append(key)
-                        one_triple.append(item[key])
-                        triples_list.append(one_triple)
-                        one_triple = []
-                        #print(f'{x}, {key}, {item[key]}\n')
-        if type(page.data['wikidata'][x]) is dict:
-            for item in page.data['wikidata'][x]:
+            #print(type(page.data['wikidata'][x]))
+            if type(page.data['wikidata'][x]) is str:
                 one_triple.append(x)
-                one_triple.append(item)
-                one_triple.append(page.data["wikidata"][x][item])
+                one_triple.append(page.data["wikidata"][x])
                 triples_list.append(one_triple)
                 one_triple = []
-                #print(f'{x}; {item}; {page.data["wikidata"][x][item]}\n')
+                #print(f'{x}; {page.data["wikidata"][x]}\n')
+            if type(page.data['wikidata'][x]) is list:
+                for item in page.data['wikidata'][x]:
+                    if type(item) is str:
+                        one_triple.append(x)
+                        one_triple.append(item)
+                        triples_list.append(one_triple)
+                        one_triple = []
+                        #print(f'{x}; {item}\n')
+                    elif type(item) is dict:
+                        for key in item:
+                            one_triple.append(x)
+                            one_triple.append(key)
+                            one_triple.append(item[key])
+                            triples_list.append(one_triple)
+                            one_triple = []
+                            #print(f'{x}, {key}, {item[key]}\n')
+            if type(page.data['wikidata'][x]) is dict:
+                for item in page.data['wikidata'][x]:
+                    one_triple.append(x)
+                    one_triple.append(item)
+                    one_triple.append(page.data["wikidata"][x][item])
+                    triples_list.append(one_triple)
+                    one_triple = []
+                    #print(f'{x}; {item}; {page.data["wikidata"][x][item]}\n')
+                    
+    except LookupError:
+        pass
                 
     return triples_list
 
@@ -142,13 +205,19 @@ def get_triples(page_name):
 def combine_triples(titles_total):
     
     triples_total = [] # All of the triples for every articles
+    counter = 0
     
     for cat in titles_total: # For each category (inner list) within titles_total
         cat_triples = [] # All of the triples for one category
-        for art in cat:
+        counter += 1
+        for art in tqdm(cat, desc=f'Retrieving triples ({counter}/{len(titles_total)})'):
             triples_list = get_triples(art)
-            cat_triples.append(triples_list)
-            print(f'{art} triples retrieved')
+            if not triples_list:
+                cat_triples.append(None)
+                print(f'{art} triples not found')
+            else:
+                cat_triples.append(triples_list)
+                print(f'{art} triples retrieved')
             
         triples_total.append(cat_triples)
         
@@ -183,7 +252,33 @@ def combine_data(categories, titles, infoboxes, content, triples):
             data_total.append(single_data) # Append all data from one article to data_total list
             
     df = pd.DataFrame(data_total, columns = ['Category', 'Title', 'Infobox', 'Content', 'Triples'])
+    
+    null_data = {}
+    null_data['Title'] = f'{df["Title"].isnull().sum()} null title items'
+    null_data['Infobox'] = f'{df["Infobox"].isnull().sum()} null infobox items'
+    null_data['Content'] = f'{df["Content"].isnull().sum()} null content items'
+    null_data['Triples'] = f'{df["Triples"].isnull().sum()} null triples items'
+    
+    group_data = df.groupby(['Category']).count()
+    
     print('Data successfully combined.')
     
-    return data_total, df
+    return data_total, group_data, null_data, df
+
+#%%
+# Testing
+
+articles_total = get_articles(categories, num_results=25)
+titles_total, infoboxes_total = get_titles_info(articles_total)
+content_total = get_content(titles_total, infoboxes_total)
+triples_total = combine_triples(titles_total)
+data_total, group_data, null_data, df = combine_data(categories, titles_total, infoboxes_total, content_total, triples_total)
+
+#%%
+
+print(df.head())
+print(group_data)
+print(null_data)
+    
+
 
