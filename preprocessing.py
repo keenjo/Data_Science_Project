b9f83ff5a8ef9a90dd39a04813449821d2ebc529
keenjo <90385673+keenjo@users.noreply.github.com>
2022-04-24 17:13:54 +0200

    New data with POS & NER

--- preprocessing.py
+++ preprocessing.py
@@ -35,11 +35,103 @@ def preprocess(text):
     # Remove stopwords and punctuation
     for sent in sp_text.sents:
         for token in sent:
-            if token.text.lower() not in STOP_WORDS and not all(char in string.punctuation for char in token.text):
+            if token.text.lower() not in STOP_WORDS and not all(char in string.punctuation for char in token.text) and not token.text.isspace():
                 tokens.append(token)
     return tokens
 
+
+def pos(tokens):
+    '''
+    Function to extract POS
+    
+    Parameters
+    ----------
+    tokens: a list of tokens
+
+    Returns
+    -------
+    pos: a list of lists in which the format of each inner list is [POS, token]
+
+    '''
+    
+    pos = []
+
+    for token in tokens:
+        pos_val = []
+        pos_val.append(token.pos_)
+        pos_val.append(token)
+        pos.append(pos_val)
+        
+    return pos
+  
+        
+def ner(text):
+    '''
+    Function for Named Entity Recognition
+    
+    Parameters
+    ----------
+    text: a string of spacy processed text
+    
+    Returns
+    -------
+    ner: a list of lists in which the format of each inner list is [NE, token]
+    '''
+    
+    sp_text = nlp(text)
+    
+    ner = []
+    
+    for word in sp_text.ents:
+        ner_val = []
+        ner_val.append(word.label_)
+        ner_val.append(word)
+        ner.append(ner_val)
+        
+    return ner
+
+
+def drop_null(df, drop_description=False):
+    '''
+    Function to drop any rows which have null text, descriptions, or triples
+    
+    Parameters
+    ----------
+    df : dataframe
+    drop_description: parameter to choose whether or not you want to drop rows that do not have a description
+
+    Returns
+    -------
+    df : processed dataframe
+
+    '''
+        
+    for row, value in enumerate(df['Content']):
+        
+        # If the 'Content' column is empty, drop the corresponding row
+        if value is None:
+            df.drop(row, axis=0, inplace=True)
+            
+        # If the 'Triples' column is empty, drop the corresponding row
+        elif df['Triples'][row] is None:
+            df.drop(row, axis=0, inplace=True)
+            
+        # If the 'Description' column is empty, drop the corresponding row [Only if 'drop_description' variable is True]
+        if drop_description == True:
+            if df['Description'][row] is None:
+                df.drop(row, axis=0, inplace=True)
+            
+    # Reset the indexes after dropping all of the values
+    df = df.reset_index(drop=True)
+            
+    return df
+
+
 df = pd.read_json('scraped_data.json')
+
+df = drop_null(df, drop_description=False)
+
+preprocessed_text = df['Content'].progress_apply(preprocess)
 
 # Convert to a new dataframe
 data = zip(
@@ -47,13 +139,17 @@ data = zip(
     df['Category'],
     df['Title'],
     df['Content'],
-    df['Content'].progress_apply(preprocess),
+    preprocessed_text,
     df['Description'],
-    df['Description'].progress_apply(preprocess)
+    df['Description'].progress_apply(preprocess),
+    preprocessed_text.progress_apply(pos),
+    df['Content'].progress_apply(ner)
 )
 
-converted_df = pd.DataFrame(data, columns=['Category number', 'Category', 'Title', 'Text', 'Processed text', 'Description', 'Processed description'])
-converted_df = converted_df.dropna()
-converted_df = converted_df.reset_index(drop=True)
+converted_df = pd.DataFrame(data, columns=['Category number', 'Category', 'Title', 'Text', 'Processed text', 'Description', 'Processed description', 'POS', 'NER'])
 converted_df.to_json('preprocessed_data.json', default_handler=str)
-#converted_df.to_json('preprocessed_data.json', default_handler=str) 
+ 
+group_data = df.groupby(['Category']).count() # The number of datapoints for each category
+
+print('>>> Grouped data <<<')
+print(group_data)
