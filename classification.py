ff4f00fdf3f0dbe1b4860aec17b436647a4a39d8
pie3636 <maxime.meloux@protonmail.com>
2022-05-04 22:57:30 +0200

    Add multi classifier comparison

--- classification.py
+++ classification.py
@@ -1,19 +1,33 @@
+print('Importing libraries...')
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy
-import sklearn
-import sklearn.feature_extraction
-import sklearn.linear_model
-import sklearn.model_selection
-import sklearn.naive_bayes
+import re
+import warnings
+
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score, classification_report, ConfusionMatrixDisplay
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import shuffle
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 # Parameters for reproducibility of the results
 RANDOM_STATE = 42
 TEST_SPLIT_SIZE = 0.25
 MAX_FEATURES = 400
-ALPHA = 0.015 # Computed using GridSearchCV (see end of the file)
-
 
 # Meta-features selected for training. Possible values are:
 # 'title', 'text', 'processed text', 'description', 'processed description', 'POS',
@@ -38,6 +52,57 @@ feature_options = {
     'NER tokens':            (False,   50),
     'processed triples':     (False,   400),
 }
+
+# List of classifiers and parameter spaces to try.
+# Please note that the initial search space was larger, but that we have reduced it
+# so that the training wouldn't last too long.
+print('Loading parameter grid...')
+classifiers = [
+    (Perceptron(random_state=RANDOM_STATE), {
+        'alpha': np.logspace(-5, 1, 20),
+        'max_iter': [5, 10, 20, 50, 100]
+    }),
+    (KNeighborsClassifier(), {
+        'n_neighbors': [3, 5, 8, 12, 20],
+        'weights': ['uniform', 'distance'],
+        'metric': ['euclidean', 'manhattan']
+    }),
+    (SVC(random_state=RANDOM_STATE), {
+        'C': np.logspace(-2, 2, 10),
+        'gamma': np.logspace(-3, 3, 10),
+        'kernel': ['rbf', 'poly', 'sigmoid']
+    }),
+    (DecisionTreeClassifier(random_state=RANDOM_STATE), {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': range(1, 10),
+        'min_samples_split': range(2, 10),
+        'min_samples_leaf': range(1, 5)
+    }),
+    (RandomForestClassifier(random_state=RANDOM_STATE), {
+        'criterion' :['gini', 'entropy'],
+        'n_estimators': list(map(int, np.logspace(1, 3, 7))),
+        'max_features': ['sqrt', 'log2'],
+        'max_depth': range(2, 10)
+    }),
+    (MLPClassifier(random_state=RANDOM_STATE), {
+        'solver': ['lbfgs'],
+        'max_iter': list(map(int, np.logspace(2, 3.5, 4))),
+        'alpha': np.logspace(-10, -1, 30),
+        'hidden_layer_sizes': list(map(int, 2**np.linspace(1, 8, 8)))
+    }),
+    (AdaBoostClassifier(random_state=RANDOM_STATE, base_estimator = DecisionTreeClassifier(random_state=RANDOM_STATE, max_features="auto", class_weight="balanced")), {
+        'base_estimator__max_depth': range(2, 10),
+        'base_estimator__min_samples_leaf': [5, 10],
+        'n_estimators': list(map(int, np.logspace(1, 3, 7))),
+        'learning_rate': np.logspace(-5, -1, 20)
+    }),
+    (GaussianNB(), {
+        'var_smoothing': np.logspace(-10, -1, 30)
+    }),
+    (QuadraticDiscriminantAnalysis(), {
+        'reg_param': np.linspace(0.1, 0.5, 5)
+    }),
+]
 
 
 def idx_to_feature(idx):
@@ -64,8 +129,9 @@ for feature in selected_features:
 
 
 # Read and shuffle input file
-df = pd.read_json('preprocessed_data.json')
-df = sklearn.utils.shuffle(df, random_state=RANDOM_STATE)
+print('Reading input file...')
+df = pd.read_json('data/preprocessed_data.json')
+df = shuffle(df, random_state=RANDOM_STATE)
 
 # Stores category names for each category index
 idx_to_category = {row['category number']: row['category'] for index, row in df.iterrows()}
@@ -73,12 +139,13 @@ idx_to_category = {row['category number']: row['category'] for index, row in df.
 vectorizers = []
 X = []
 
+print('Running input through TfidfVectorizer...')
 
-# Main training loop. We train a different vectorizer (with different parameters) for each meta-feature.
+# Vectorizer training loop. We train a different vectorizer (with different parameters) for each meta-feature.
 # The input X of the perceptron is created by stacking the individual inputs X for each meta-features.
 for column_name in selected_features:
     (use_idf, max_features) = feature_options[column_name]
-    vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(max_features=max_features, use_idf=use_idf, stop_words='english')
+    vectorizer = TfidfVectorizer(max_features=max_features, use_idf=use_idf, stop_words='english')
     vectorizers.append(vectorizer)
     
     # TfidfVectorizer only accepts lists of strings as input (and not lists of lists of words)
@@ -87,23 +154,62 @@ for column_name in selected_features:
     # Create the partial input for the current meta-feature
     X_partial = vectorizer.fit_transform(texts)
     X.append(X_partial)
-    
 
 # Create the final input
 X = scipy.sparse.hstack(X)
 Y = df['category number']
 
-perceptron = sklearn.linear_model.Perceptron(random_state=RANDOM_STATE, alpha=ALPHA)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=TEST_SPLIT_SIZE, random_state=RANDOM_STATE)
 
+best_classifiers = []
+display_strings = []
 
-# Split 'n fit
-X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=TEST_SPLIT_SIZE, random_state=RANDOM_STATE)
-perceptron.fit(X_train, Y_train)
+print('Optimizing hyperparameters for all models...')
+print('Warning: This operation can take a long time (~1 hour). Results will be displayed at the end.')
 
+# Classifier optimization loop
+for i, (classifier, parameters) in enumerate(classifiers):
 
-# Predict and display performance metrics
-Y_pred = perceptron.predict(X_test)
+    # For each classifier, we first create a GridSearchCV object for the desired parameters
+    clf = GridSearchCV(estimator=classifier, param_grid=parameters, scoring='accuracy', return_train_score=True, cv=5, verbose=2)
 
+    # Some classifiers don't accept sparse matrices as input
+    X_model_train = X_train
+    if isinstance(classifier, (GaussianProcessClassifier, GaussianNB, QuadraticDiscriminantAnalysis)):
+        X_model_train = X_model_train.toarray()
+    
+    # Compute the optimal hyperparameters
+    clf.fit(X_model_train, Y_train)
+    best_parameters = clf.best_estimator_.get_params()
+    
+    # Store best classifier and accuracy
+    best_classifiers.append((clf.best_score_, clf.best_estimator_))
+    
+    display_strings.append([
+        str(classifier.__class__).ljust(35),                          # Classifier name
+        str({k: best_parameters[k] for k in parameters}).ljust(100), # All optimized parameters
+        clf.best_score_                                              # Accuracy
+    ])
+    print(f'Optimizing hyperparameters for all models ({i+1}/{len(classifiers)})...')
+
+print()
+print('Classifier'.ljust(35), 'Best parameters'.ljust(100), 'Best accuracy')
+print('-'*145)
+
+for s1, s2, s3 in display_strings:
+    print(s1, s2, s3)
+
+# Sort classifiers by accuracy and pick the best one
+best_accuracy, best_classifier = sorted(best_classifiers, reverse=True)[0]
+
+Y_pred = best_classifier.predict(X_test.toarray())
+
+print('Best classifier', best_classifier)
+print('Training accuracy', best_accuracy)
+print('Test accuracy', accuracy_score(Y_test, Y_pred))
+print()
+
+# Show predicted vs expected classes
 print('Predictions:')
 for pred in Y_pred:
     print(str(pred).ljust(3), end='')
@@ -114,14 +220,10 @@ for pred in Y_test:
     print(str(pred).ljust(3), end='')
 print()
 
-# Accuracy
-print('Accuracy: ', sklearn.metrics.accuracy_score(Y_test, Y_pred))
-
 # Confusion matrix (opens in a graphical window)
-sklearn.metrics.ConfusionMatrixDisplay.from_predictions(Y_test, Y_pred)
+ConfusionMatrixDisplay.from_predictions(Y_test, Y_pred)
 
-# Metrics for each class
-print(sklearn.metrics.classification_report(Y_test, Y_pred))
+print(classification_report(Y_test, Y_pred))
 
 plt.show()
 
@@ -133,30 +235,12 @@ for i, vectorizer in enumerate(vectorizers):
     for k, v in vectorizer.vocabulary_.items():
         idx_to_tag[feature_idx_offset[selected_features[i]] + v] = k
 
-# Display top features
-print('Category'.ljust(30), 'Meta-feature'.ljust(30), 'Feature'.ljust(20), 'Weight'.ljust(20))
-print('-'*100)
-for class_num, class_weights in enumerate(perceptron.coef_):
-    # Compute and display the top features and weights for the class
-    top_vals = sorted(enumerate(class_weights), reverse=True, key=lambda x: x[1])
-    for id, val in top_vals[:10]:
-        print(idx_to_category[class_num].ljust(30), idx_to_feature(id).ljust(30), idx_to_tag[id].ljust(20), val.round(3))
+# # Display top features
+# print('Category'.ljust(30), 'Meta-feature'.ljust(30), 'Feature'.ljust(20), 'Weight'.ljust(20))
+# print('-'*100)
+# for class_num, class_weights in enumerate(perceptron.coef_):
+    # # Compute and display the top features and weights for the class
+    # top_vals = sorted(enumerate(class_weights), reverse=True, key=lambda x: x[1])
+    # for id, val in top_vals[:10]:
+        # print(idx_to_category[class_num].ljust(30), idx_to_feature(id).ljust(30), idx_to_tag[id].ljust(20), val.round(3))
 
-
-# GridSearchCV, used for finding the best `alpha` parameter for the perceptron.
-parameters = [a for a in np.logspace(-5, 1, 50)]
-clf = sklearn.model_selection.GridSearchCV(estimator=sklearn.naive_bayes.MultinomialNB(), 
-                   param_grid={'alpha': parameters},
-                   scoring='accuracy',
-                   return_train_score=True,
-                   cv=5
-                  )
-
-clf.fit(X_train, Y_train)
-best_parameters = clf.best_estimator_.get_params()
-
-cv_res = pd.DataFrame(clf.cv_results_)
-#print(cv_res)
-
-print("Best GridSearchCV score: %0.3f" % clf.best_score_)
-print("Best GridSearchCV alpha", best_parameters['alpha'])
